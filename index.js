dad63b229aff9de0e59147bd169b0e68a7e60ae8
THENGARAIMAHARA\theng <tmaharajan1999@gmail.com>
2024-06-01 23:18:30 +0530

    update and delete a particular product complete

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/',(req,res)=>{
     res.send("Hello from Node API");
 });
 
+// View All Products
 app.get('/api/products', async (req,res)=>{
     try{
       const products = await Product.find({});
@@ -18,7 +19,8 @@ app.get('/api/products', async (req,res)=>{
     }
 });
 
-app.get('/api/products/:id', async (req,res)=>{
+// View An individual Product
+app.get('/api/product/:id', async (req,res)=>{
     try{
         const { id } = req.params;
         const product = await Product.findById(id);
@@ -29,6 +31,7 @@ app.get('/api/products/:id', async (req,res)=>{
     }
 });
 
+// Add products
 app.post('/api/products', async (req,res)=>{
     try{
         const product = await Product.create(req.body);
@@ -38,6 +41,38 @@ app.post('/api/products', async (req,res)=>{
         res.status(500).json({message:error.message});
     }
 });
+
+// update a product
+app.put('/api/product/:id', async (req,res)=>{
+    try{
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id,req.body);
+        if(!product){
+            return res.status(404).json({message : "Product Not Found"});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    }
+    catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+// delete a product
+app.delete('/api/product/:id', async (req,res)=>{
+    try{
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message : "product not found"});
+        }
+        res.status(200).json({message : "product Deleted Successfully"});
+    }
+    catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
 
 mongoose.connect('mongodb+srv://Admin:Admin@simplecrudapp.46gvvy8.mongodb.net/Node-API')
   .then(() => {
