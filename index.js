55d4a3335ef0a72e6c3de47b00c96e21a542fcc9
THENGARAIMAHARA\theng <tmaharajan1999@gmail.com>
2024-06-01 23:25:18 +0530

    url encoded

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
     res.send("Hello from Node API");
